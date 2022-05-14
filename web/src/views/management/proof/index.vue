3a4da609920d8dc2de6571b6c3625f508a608538
JosephXue2020 <pimbury@qq.com>
2022-05-14 16:11:23 +0800

    修改了前端ui

---
+++ web/src/views/management/proof/index.vue
@@ -0,0 +1,30 @@
+<template>
+  <div class="management-container">
+    <div class="management-text">专家审核</div>
+  </div>
+</template>
+
+<script>
+import { mapGetters } from 'vuex'
+
+export default {
+  name: 'Proof',
+  computed: {
+    ...mapGetters([
+      'name'
+    ])
+  }
+}
+</script>
+
+<style lang="scss" scoped>
+.management {
+  &-container {
+    margin: 30px;
+  }
+  &-text {
+    font-size: 30px;
+    line-height: 46px;
+  }
+}
+</style>
